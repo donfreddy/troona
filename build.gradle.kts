f24339c61c9f4a9dcc28b1d13c4ad270837104ac
Don Freddy <freddy.tamwo@neero.io>
2024-01-09 14:15:56 +0100

    chore: add a header license

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,18 @@
 /*
- * Author: Don Freddy <freddytamwo@gmail.com>
- * Github: https://github.com/donfreddy
- * Website: https://donfreddy.com
- *
- * Application: Troona
- * Homepage: https://github.com/donfreddy/troona
- * License: https://github.com/donfreddy/troona/blob/main/LICENSE
- * Copyright: © 2023, Don Freddy. All rights reserved.
- */
+=============
+Author: Don Freddy
+Github: https://github.com/donfreddy
+Website: https://donfreddy.com
+=============
+Application: Troona - Music Player
+Homepage: https://github.com/donfreddy/troona
+License: https://github.com/donfreddy/troona/blob/main/LICENSE
+Copyright: © 2023, Don Freddy. All rights reserved.
+=============
+*/
 
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-  alias(libs.plugins.androidApplication) apply false
-  alias(libs.plugins.jetbrainsKotlinAndroid) apply false
+  alias(libs.plugins.android.application) apply false
+  alias(libs.plugins.kotlin.android) apply false
 }
