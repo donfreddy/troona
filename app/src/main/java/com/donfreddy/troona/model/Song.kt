4d7c7b9cd2bb643206c5c2008304e7c9f31f914b
Don Freddy <freddy.tamwo@neero.io>
2024-01-09 14:35:54 +0100

    chore: add song model

---
+++ app/src/main/java/com/donfreddy/troona/model/Song.kt
@@ -0,0 +1,25 @@
+/*
+=============
+Author: Don Freddy
+Github: https://github.com/donfreddy
+Website: https://donfreddy.com
+=============
+Application: Troona - Music Player
+Homepage: https://github.com/donfreddy/troona
+License: https://github.com/donfreddy/troona/blob/main/LICENSE
+Copyright: © 2023, Don Freddy. All rights reserved.
+=============
+*/
+
+package com.donfreddy.troona.model
+
+data class Song(
+    val id: Long,
+    val title: String,
+    val artist: String,
+    val album: String,
+    val duration: Long,
+    val path: String,
+    val albumId: Long,
+    val albumArt: String
+)
