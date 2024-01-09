285d47e4dd5d4def118c2abb75e74b35418489d1
Don Freddy <freddy.tamwo@neero.io>
2024-01-09 14:49:14 +0100

    chore: add enum util file

---
+++ app/src/main/java/com/donfreddy/troona/util/Enums.kt
@@ -0,0 +1,19 @@
+/*
+=============
+Author: Don Freddy
+Github: https://github.com/donfreddy
+Website: https://donfreddy.com
+=============
+Application: Troona - Music Player
+Homepage: https://github.com/donfreddy/troona
+License: https://github.com/donfreddy/troona/blob/main/LICENSE
+Copyright: © 2023, Don Freddy. All rights reserved.
+=============
+*/
+
+package com.donfreddy.troona.util
+
+enum class SearchWidgetState(val value: String) {
+  OPENED(value = "opened"),
+  CLOSED(value = "closed")
+}
